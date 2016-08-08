2381b3cfaef15f61720c8cabda04277810fdd677
zhuweili <weilizhu@buffalo.edu>
2016-08-08 01:17:18 -0400

    07/08/2016

---
+++ server.js
@@ -0,0 +1,16 @@
+/**
+ * Created by user on 8/8/16.
+ */
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+        console.log(" requrest once");
+        res.json( {
+            name: "weilizhu",
+            age: 27,
+            major: "CSE"
+        })
+});
+
+app.listen(3000);
