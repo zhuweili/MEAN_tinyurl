6d2a9d0bd4c8f3aad6fadf20d0724ac4921229be
zhuweili <weilizhu@buffalo.edu>
2016-08-18 14:55:07 -0400

    18/08/2016 UI setup

--- public/javascripts/controllers/homeController.js
+++ public/javascripts/controllers/homeController.js
@@ -3,11 +3,14 @@
  */
 var app = angular.module("tinyurlApp");
 
-app.controller("homeController", ["$scope", "$http",function($scope, $http) {
+app.controller("homeController", ["$scope", "$http","$location",function($scope, $http, $location) {
     $scope.submit = function() {
         console.log($scope.longUrl);
         $http.post ("/api/v1/urls", {
             longUrl: $scope.longUrl
-        });
+        })
+            .success(function(data) {
+                  $location.path("/urls/" +data.shortUrl);
+            });
     }
 }]);
