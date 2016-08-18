a19cd01ddc0dbfc7be1fc93aaf5c2faece56f3d9
zhuweili <weilizhu@buffalo.edu>
2016-08-18 02:37:12 -0400

    18/08/2016 UI setup

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.sendFile('./public/views/index.html');
+  res.sendFile('index.html', { root: './public/views'});
 });
 
 module.exports = router;
