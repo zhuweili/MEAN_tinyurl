084b81ca6bb490cb479525870718645fe4ddd893
zhuweili <weilizhu@buffalo.edu>
2016-08-18 04:29:51 -0400

    18/08/2016 UI setup

--- public/javascripts/controllers/homeController.js
+++ public/javascripts/controllers/homeController.js
@@ -1,3 +1,10 @@
 /**
  * Created by user on 8/18/16.
  */
+var app = angular.module("tinyurlApp");
+
+app.controller("homeController", ["$scope", function($scope) {
+    $scope.submit = function() {
+        console.log("hahhahahahah");
+    }
+}]);
