8c99efb69d7c71774e6b8961726f49b9f5b91a31
zhuweili <weilizhu@buffalo.edu>
2016-08-18 02:10:05 -0400

    18/08/2016

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ var express = require('express');
 var app = express();
 var restRouter = require('./routes/rest');
 var redirectRouter = require('./routes/redirect');
+var indexRouter = require('./routes/index');
 
 app.longToShortHash = {};
 app.shortToLongHash = {};
 
 app.use('/api/v1', restRouter);
-
+app.use('/', indexRouter);
 app.use('/:shortUrl', redirectRouter);
 
 app.listen(3000);
